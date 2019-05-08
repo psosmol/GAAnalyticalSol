ead04f103b2b11381361bdfa5818bf9ee84fd634
psosmol <50246772+psosmol@users.noreply.github.com>
2019-05-08 20:21:15 +0200

    Add files via upload

---
+++ defs.h
@@ -0,0 +1,11 @@
+#ifndef DEFS_H
+#define DEFS_H
+
+#define MAX_POP 10
+#define CHROM_SIZE 35
+#define POP_SIZE 100
+#define MUTATION_RATE 0.01
+#define CROSSOVER_RATE 0.95
+#define ELITE_COUNT 10
+
+#endif
